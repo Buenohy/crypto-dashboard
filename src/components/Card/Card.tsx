a1ba8d1deee7b21a14688ec8a90964543bb14d03
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-08-24 20:27:16 -0300

    Change code to resolve error log

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { FaBtc } from 'react-icons/fa6';
+import { FaBtc, FaEthereum } from 'react-icons/fa6';
+import { SiLitecoin, SiSolana } from 'react-icons/si';
 import { RiArrowUpSFill } from 'react-icons/ri';
 import ChartLineLinear from '../CardChartShadcnUi/CardChartShadcnUi';
 
@@ -12,17 +13,43 @@ interface CardProps {
   image: string;
 }
 
+const getCryptoIcon = (subtitle: string) => {
+  switch (subtitle.toUpperCase()) {
+    case 'BTC':
+      return (
+        <FaBtc className="text-yellow-300 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+      );
+    case 'ETH':
+      return (
+        <FaEthereum className="text-blue-400 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+      );
+    case 'LTC':
+      return (
+        <SiLitecoin className="text-gray-400 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+      );
+    case 'SOL':
+      return (
+        <SiSolana className="text-purple-400 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+      );
+    default:
+      return (
+        <FaBtc className="text-yellow-300 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+      );
+  }
+};
+
 export default function Card({
   title,
   subtitle,
   value,
   percentage,
+  image,
 }: CardProps) {
   return (
     <div className="bg-white dark:bg-bg-blue rounded-xl shadow-xl px-4 py-6 w-full h-[178px] 2xl:h-[278px] ">
       <div className="flex gap-4 items-center justify-between mb-5 2xl:mb-10">
         <div className="flex gap-5">
-          <FaBtc className="text-yellow-300 p-2 bg-text-gray dark:bg-bg-gray w-11 h-11 2xl:w-16 2xl:h-16 rounded-lg shadow-lg" />
+          {getCryptoIcon(subtitle)}
           <div className="flex flex-col">
             <span className="text-black dark:text-white text-base 2xl:text-3xl font-semibold">
               {title}
@@ -46,7 +73,6 @@ export default function Card({
           </span>
         </div>
         <div className="w-[108px] h-[44px] 2xl:w-[138px] 2xl:h-[84px]">
-          {/* <CardChart /> */}
           <ChartLineLinear />
         </div>
       </div>
